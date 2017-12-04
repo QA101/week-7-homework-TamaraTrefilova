03929dc0588300bf1e906dbec8733d1786b1cadc
Tamara Trefilova <tamara.trefilova@gmail.com>
2017-12-03 17:10:02 -0800

    adding files

--- tst/greetingsUtil/GreetingsUtilTest.java
+++ tst/greetingsUtil/GreetingsUtilTest.java
@@ -4,32 +4,45 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class GreetingsUtilTest {
-	static GreetingsUtil gu;
-	
+	static GreetingsUtil gu;	
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		gu = new GreetingsUtil();
 	}
 
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-
 	@Before
 	public void setUp() throws Exception {
+		gu.ChangeState();
 	}
-
-
-	@After
-	public void tearDown() throws Exception {
+	
+	@Test
+	public void testHello() {
+		System.out.println("Hello");
+		assertEquals(gu.currentState, "going");
+		assertEquals(gu.Hello(),"Hi!");
+		assertEquals(gu.currentState, "coming");
+		assertEquals(gu.Hello(),"You already said that");
+		assertEquals(gu.currentState, "coming");
 	}
 
 	@Test
-	public void test() {
-		fail("your tests here");
+	public void testGoodbye() {	
+		System.out.println("Goodbye");
+		assertEquals(gu.currentState, "coming");
+		assertEquals(gu.GoodBye(),"bye");
+		assertEquals(gu.currentState, "going");
+		assertEquals(gu.GoodBye(),"You already said that");		
+		assertEquals(gu.currentState, "going");
+	}
+	
+	@After
+	public void tearDown() throws Exception {	
+		gu.ChangeState();
 	}
 
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		gu = null;
+	}
 }
---
+++ tst/greetingsUtil/SuiteClass.java
@@ -0,0 +1,14 @@
+package greetingsUtil;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	GreetingsUtilTest.class})
+
+	public class SuiteClass {
+
+}
